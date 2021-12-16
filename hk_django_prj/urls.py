671a7acde7721da6cb4b4fa1cc0c1f5f38c8b356
strongsm <96212502+strongsm@users.noreply.github.com>
2021-12-17 01:14:27 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- hk_django_prj/urls.py
+++ hk_django_prj/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
